c8fcefc34b5eda06f198f851e9fefdd6b674289e
Martin <martinmacdonald1@live.co.uk>
2020-09-11 16:37:19 +0100

    add scss module and initial styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,15 +6,41 @@
  */
 
 import React from "react"
-
+import "./layout.scss"
 
 const Layout = () => {
   return (
-    <header>
-      <h1>My name is Martin MacDonald</h1>
-    </header>
+    <div>
+      <header>
+        <h1>My name is Martin MacDonald.</h1>
+      </header>
+      <div className="boxes">
+        <div className="box">
+          <img
+            src="https://images.pexels.com/photos/4526398/pexels-photo-4526398.jpeg?cs=srgb&dl=pexels-screen-post-4526398.jpg&fm=jpg"
+            alt=""
+          />
+          <div className="box-content">Box1 </div>
+        </div>
+        <div className="box">
+          <img
+            src="https://images.pexels.com/photos/193349/pexels-photo-193349.jpeg?cs=srgb&dl=pexels-markus-spiske-193349.jpg&fm=jpg"
+            alt=""
+          />
+
+          <div className="box-content">Box2</div>
+        </div>
+        <div className="box">
+          <img
+            src="https://images.pexels.com/photos/3756879/pexels-photo-3756879.jpeg?cs=srgb&dl=pexels-sound-on-3756879.jpg&fm=jpg"
+            alt=""
+          />
+
+          <div className="box-content">Box3 </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default Layout
- 
